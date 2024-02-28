36c8df5d4ec2bf16e143ded35900b693bfac6491
JangsanHighDev <mocakosan96@highdev.pro>
2024-02-28 18:33:07 +0900

    sanToon AppHear making

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+  Player({required this.name});
+}
+
 void main() {
+  var nico = Player(name: "potato");
   runApp(App());
 }
 
 class App extends StatelessWidget {
-
+  const App({super.key});
   @override
   Widget build(BuildContext context) {
-   return
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
