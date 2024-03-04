57bd5cea0445f78657d1807d5aba9aa954976565
JangsanHighDev <mocakosan96@highdev.pro>
2024-03-04 19:51:32 +0900

    flutter UI practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,65 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;
-  Player({required this.name});
-}
+// class Player {
+//   String? name;
+//   Player({required this.name});
+// }
 
 void main() {
-  var nico = Player(name: "potato");
+  // var nico = Player(name: "potato");
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  // const App({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, San',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome ',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8))),
+              const SizedBox(height: 5)
+            ],
+          ),
         ),
       ),
     );
